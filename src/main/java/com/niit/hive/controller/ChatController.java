112587f9d1763b12e9fe33a7ae55ceb6fcc6b385
PranilPathareProject2 <pranilpathare93@gmail.com>
2017-01-22 01:12:56 +0530

    Second Project

--- src/main/java/com/niit/hive/controller/ChatController.java
+++ src/main/java/com/niit/hive/controller/ChatController.java
@@ -2,8 +2,11 @@ package com.niit.hive.controller;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
 import com.niit.hive.model.Message;
@@ -11,11 +14,18 @@ import com.niit.hive.model.OutputMessage;
 
 @Controller
 public class ChatController {
+
+	@Autowired
+	private SimpMessagingTemplate template;
+
+    public ChatController(SimpMessagingTemplate template) {
+        this.template = template;
+    }
 	
 	@MessageMapping("/chat") //to send message
-	@SendTo("/queue/message") //to receive message
-	public OutputMessage sendReceiveMessage(Message message)
+	@SendToUser("/user/queue/message") //to receive message
+	public void sendReceiveMessage(Message message)
 	{
-		return new OutputMessage(message, new Date());
+		this.template.convertAndSendToUser(message.getFriend_id(), "/queue/message", new OutputMessage(message, new Date()));
 	}
 }
--- src/main/java/com/niit/hive/model/Message.java
+++ src/main/java/com/niit/hive/model/Message.java
@@ -4,8 +4,18 @@ public class Message {
 
 	private String message;
 	
-	public Message(String message) {
+	private int message_id;
+	
+	private String friend_id;
+	
+	public Message() {
+		
+	}
+	
+	public Message(String message, int message_id, String friend_id) {
 		this.message = message;
+		this.message_id = message_id;
+		this.friend_id = friend_id;
 	}
 
 	public String getMessage() {
@@ -14,5 +24,21 @@ public class Message {
 
 	public void setMessage(String message) {
 		this.message = message;
+	}
+
+	public int getMessage_id() {
+		return message_id;
+	}
+
+	public void setMessage_id(int message_id) {
+		this.message_id = message_id;
+	}
+
+	public String getFriend_id() {
+		return friend_id;
+	}
+
+	public void setFriend_id(String friend_id) {
+		this.friend_id = friend_id;
 	}
 }
